ab70758b8587ab1256e1879a836bcb91e949785a
RuanVFeitosa <vianafeitosaruan@gmail.com>
2025-12-16 21:00:34 -0300

    Criacao das paginas Sobre Nos, Servicos e Atuacao Global + link GitPages

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
-// main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // ← Importe BrowserRouter
 import './index.css'
 import App from './pages/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/projeto-phrima"> {/* ← Adicione basename */}
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
